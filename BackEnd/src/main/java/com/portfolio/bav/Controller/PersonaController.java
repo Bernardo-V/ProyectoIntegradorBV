bd4f4723ff337efc7c9493a896cd57533e775496
Bernardo A V <profesorbernardov@gmail.com>
2022-07-04 21:44:59 -0300

    Creando Home y Login
    Creando Home y Login

--- BackEnd/src/main/java/com/portfolio/bav/Controller/PersonaController.java
+++ BackEnd/src/main/java/com/portfolio/bav/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.bav.Entity.Persona;
 import com.portfolio.bav.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -28,24 +30,28 @@ public class PersonaController {
         return "La persona fué creada correctamente";
     }
     
-    @DeleteMapping("/personas/borrar/(id)")
+    @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "La persona fué eliminada";
     }
-    
-    @PutMapping("/personas/editar/(id)")
+    //URL:Puerto/personas/editar/id/nombre&apellido&imagen
+    @PutMapping("/personas/editar/{id}")
     public Persona editPersona (@PathVariable Long id,
                                 @RequestParam("nombre") String nuevoNombre,
                                 @RequestParam("apellido") String nuevoApellido,
                                 @RequestParam("img") String nuevoImg){
-            Persona persona = ipersonaService.findPersona(id);
-            
+        Persona persona = ipersonaService.findPersona(id);
+         
             persona.setNombre(nuevoNombre);
             persona.setApellido(nuevoApellido);
             persona.setImg(nuevoImg);
             
             ipersonaService.savePersona(persona);
             return persona;
+    }
+    @GetMapping("personas/traer/perfil")
+    public Persona findPersona(){
+     return ipersonaService.findPersona((long)1);
     }
 }
--- BackEnd/src/main/java/com/portfolio/bav/Interface/IPersonaService.java
+++ BackEnd/src/main/java/com/portfolio/bav/Interface/IPersonaService.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 
 public interface IPersonaService {
+    //Traer una lista de personas
     public List<Persona> getPersona();
+    //Guardar un objeto de tipo persona
     public void savePersona (Persona persona);
+    //Eliminar persona pero lo buscamos por ID
     public void deletePersona (Long id);
+    //Buscar una persona por ID
     public Persona findPersona (Long id);
 }
